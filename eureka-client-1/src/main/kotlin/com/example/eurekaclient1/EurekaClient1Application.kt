f0f800d6a81079b0e274fe8045a3090d77042374
junho <wnsgh1357@gmail.com>
2023-06-15 05:23:43 +0900

    Initial commit

---
+++ eureka-client-1/src/main/kotlin/com/example/eurekaclient1/EurekaClient1Application.kt
@@ -0,0 +1,11 @@
+package com.example.eurekaclient1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EurekaClient1Application
+
+fun main(args: Array<String>) {
+	runApplication<EurekaClient1Application>(*args)
+}
---
+++ eureka-client-2/src/main/kotlin/com/example/eurekaclient2/EurekaClient2Application.kt
@@ -0,0 +1,11 @@
+package com.example.eurekaclient2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EurekaClient2Application
+
+fun main(args: Array<String>) {
+	runApplication<EurekaClient2Application>(*args)
+}
